154d5a54f419dea2a0bb51f6386a290490593034
Mrbillybobby <107970323+Mrbillybobby@users.noreply.github.com>
2022-06-21 23:37:34 -0700

    Add files via upload

---
+++ Anthony Q About Me/script.js
@@ -0,0 +1,41 @@
+let whoArePage = document.getElementById("whoAreYou")
+let whoWannaPage = document.getElementById("whoIWanna")
+let hobbiesPage = document.getElementById("hobbies")
+
+    
+let firstBtn = document.getElementById("buttonOne")
+let secBtn = document.getElementById("buttonTwo")
+let thirdBtn = document.getElementById("buttonThree")
+
+
+firstBtn.addEventListener("click", () => { 
+	changePage(1)
+})
+
+secBtn.addEventListener("click", () => {
+	changePage(2)
+})
+thirdBtn.addEventListener("click", () => {
+	changePage(3)
+})
+
+function changePage(pageNumber) {
+    if (pageNumber == 1) {
+    	whoArePage.style.display = "block";
+      	whoWannaPage.style.display = "none";
+      	hobbiesPage.style.display = "none";
+		
+      	
+    } else if (pageNumber == 2) {
+        whoArePage.style.display = "none";
+        whoWannaPage.style.display = "block";
+        hobbiesPage.style.display = "none";
+		  	
+    } else if (pageNumber == 3) {
+        whoArePage.style.display = "none";
+        whoWannaPage.style.display = "none";
+        hobbiesPage.style.display = "block";
+	
+    }
+
+}
